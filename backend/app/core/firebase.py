9abc4d40bc81534cc7b4e601322dbf1fa67cb7a2
setsuwww <setsuprojectonly@gmail.com>
2026-03-18 00:48:18 +0700

    Firebase config clear

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("app/core/artilia-key-config.json")
+    cred = credentials.Certificate("app/core/firebase.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
